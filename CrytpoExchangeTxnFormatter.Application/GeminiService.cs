327f5d5b6784540667c42145172111850b83e5d0
Kevin Herr <kherr9@gmail.com>
2018-03-15 17:28:13 -0500

    stub project

---
+++ CrytpoExchangeTxnFormatter.Application/GeminiService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace CrytpoExchangeTxnFormatter.Application
+{
+    public class GeminiService
+    {
+        public Stream Convert(Stream source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
